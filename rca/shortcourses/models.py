1789f54f19264b9aeb6253cb2e5bfbc860f0824e
kevinhowbrook <kbhowbrook@fastmail.com>
2020-03-12 14:28:05 +0000

    WIP parsing

--- rca/shortcourses/models.py
+++ rca/shortcourses/models.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup as Soup
 from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.search import index
@@ -17,3 +19,18 @@ class ShortCoursePage(BasePage):
     content_panels = BasePage.content_panels + [
         FieldPanel("introduction"),
     ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        url = (
+            "https://rca.accessplanit.com/accessplansandbox/services/WebIntegration.asmx/GetCoursesPackage?companyID"
+            "=ROYALC9RCH&courseIDs=731014&venueIDs=&categoryIDs="
+        )
+        response = requests.get(url)
+        soup = Soup(response.text, "lxml")
+        for date in soup.findAll("dates"):
+            print(date)
+        context["ap_data"] = soup
+
+        return context
